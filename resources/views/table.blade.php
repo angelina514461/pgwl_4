deeea786b89ee42a5128c3135a1053ab9d7605af
angelina514461 <angelinadyahramadhani@mail.ugm.ac.id>
2025-03-12 11:29:05 +0700

    Initial commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <head>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Kelas</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Angel</td>
+            <td>611</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Kaysen</td>
+            <td>612</td>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Ell</td>
+            <td>613</td>
+            <td>A</td>
+        </tr>
+    </head>
+</table>
+@endsection
